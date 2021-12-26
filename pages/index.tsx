fec6230810016c5cdc26b3a1793a794295442b95
Pankaj <pankajmlv@gmail.com>
2021-12-26 17:02:20 +0530

    fix mapp position

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,13 @@ const Home: NextPage = () => {
       </main>
     </div>
     <footer className={styles.footer}>
-       <div className="row ml-5">
-         <div className="col-6 col-md-6">
+       <div className="row ml-5 d-flex">
+         <div className="col-6 col-md-6 col-sm-12 col-xs-12">
            <ContactForm/>
          </div>
-         <div className="col-6 col-md-6">
-         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2455.5618407616425!2d75.84059805503514!3d22.758491220062737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396303e32565f7b3%3A0x30130836204e1dba!2sThe%20Saras%20Engineers!5e0!3m2!1sen!2sin!4v1640515582626!5m2!1sen!2sin" height="250vh" width="500vw" loading="lazy"></iframe>
+         <div className="col-6 col-md-6 col-sm-12 col-xs-12">
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2455.5618407616425!2d75.84059805503514!3d22.758491220062737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396303e32565f7b3%3A0x30130836204e1dba!2sThe%20Saras%20Engineers!5e0!3m2!1sen!2sin!4v1640515582626!5m2!1sen!2sin" 
+         height="250vh" width="500vw" loading="lazy"></iframe>
          </div>
         
        </div>
