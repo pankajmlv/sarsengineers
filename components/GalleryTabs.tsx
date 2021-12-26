930b9c733543cac324fa0bdbb755d032c671992c
Pankaj <pankajmlv@gmail.com>
2021-12-26 19:16:30 +0530

    add tab icon

--- components/GalleryTabs.tsx
+++ components/GalleryTabs.tsx
@@ -1,6 +1,41 @@
-import { Paper, Tabs, Tab } from '@mui/material';
+import { Paper, Tabs, Tab, Box, Typography } from '@mui/material';
 import React from 'react';
 import { makeStyles } from '@mui/styles';
+import PropTypes from 'prop-types';
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
+import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
+function TabPanel(props: any) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`scrollable-force-tabpanel-${index}`}
+        aria-labelledby={`scrollable-force-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  
+  function a11yProps(index: any) {
+    return {
+      id: `scrollable-force-tab-${index}`,
+      'aria-controls': `scrollable-force-tabpanel-${index}`,
+    };
+  }
 
 const useStyles: any = makeStyles({
   root: {
@@ -17,6 +52,7 @@ export default function CenteredTabs() {
   };
 
   return (
+      <>
     <Paper className={classes.root}>
       <Tabs
         value={value}
@@ -25,9 +61,16 @@ export default function CenteredTabs() {
         textColor="primary"
         centered
       >
-        <Tab label="Images" />
-        <Tab label="Video" />
+        <Tab label="Images" icon={<PhotoCameraIcon/>} {...a11yProps(0)}/>
+        <Tab label="Videos" icon={<VideoCameraFrontIcon/>} {...a11yProps(1)}/>
       </Tabs>
     </Paper>
+    <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+    </>
   );
 }
