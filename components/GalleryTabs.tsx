1e3c9ab10071df558203cd397efb5adcfd417823
Pankaj <pankajmlv@gmail.com>
2021-12-26 20:05:47 +0530

    add images and videos

--- components/GalleryTabs.tsx
+++ components/GalleryTabs.tsx
@@ -1,9 +1,10 @@
-import { Paper, Tabs, Tab, Box, Typography } from '@mui/material';
+import { Paper, Tabs, Tab, Box, Typography, ImageList, ImageListItem } from '@mui/material';
 import React from 'react';
 import { makeStyles } from '@mui/styles';
 import PropTypes from 'prop-types';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
+
 function TabPanel(props: any) {
     const { children, value, index, ...other } = props;
   
@@ -66,11 +67,28 @@ export default function CenteredTabs() {
       </Tabs>
     </Paper>
     <TabPanel value={value} index={0}>
-        Item One
+    <ImageList variant="masonry" cols={3} gap={8}>
+  {imgData.map((img) => (
+    <ImageListItem key={img}>
+      <img
+        src={`${path}${img}.jpg?w=248&fit=crop&auto=format`}
+        srcSet={`${path}${img}.jpg?w=248&fit=crop&auto=format&dpr=2 2x`}
+        alt="no img"
+        loading="lazy"
+      />
+    </ImageListItem>
+  ))}
+</ImageList>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item Two
+          <div className='row justify-content-center'>
+      <video width="320" height="240" src={`../static/videos/${videoData[0]}.mp4`} controls></video>
+      </div>
       </TabPanel>
     </>
   );
 }
+
+const imgData = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37];
+const videoData = [1];
+const path = '../static/images/'
--- pages/about.tsx
+++ pages/about.tsx
@@ -18,11 +18,18 @@ const About: NextPage = () => {
       </main>
     </div>
     <footer className={styles.footer}>
-       <div className="row ml-5 d-flex">
-         <div className="col-12 col-md-6 col-sm-12">
+       <div className="row">
+       <div className="col-12 col-md-4 col-sm-12 mb-2" style={{color: '#ffffff', fontWeight: 'bold'}}>
+         The Saras Engineers <br/>
+         Mobile no. 8982227228 <br/>
+         thesarasengineers@gmail.com <br/>
+         Plot no.370 - gali no. 3 New bajrangpura Sanwer road 452015 <br/>
+         GST NO. - 23CQPPP9512D1ZP <br/>
+         </div>
+         <div className="col-12 col-md-4 col-sm-12 mb-2">
            <ContactForm/>
          </div>
-         <div className="col-12 col-md-6 col-sm-12 ">
+         <div className="col-12 col-md-4 col-sm-12 mb-2">
          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2455.5618407616425!2d75.84059805503514!3d22.758491220062737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396303e32565f7b3%3A0x30130836204e1dba!2sThe%20Saras%20Engineers!5e0!3m2!1sen!2sin!4v1640515582626!5m2!1sen!2sin" 
          height="250vh" loading="lazy"></iframe>
          </div>
